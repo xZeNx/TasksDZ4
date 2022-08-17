25f06c6538a528f8057a555d1bb37dbf8b27af16
ZeN <zensmi@yandex.ru>
2022-08-17 14:50:21 +0300

    DZ

---
+++ DZ/DZ_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
+
+int degree(int numberA, int numberB)
+{
+  int result = 1;
+  for(int i=1; i <= numberB; i++)
+  {
+    result = result * numberA;
+  }
+    return result;
+}
+  Console.Write("Ведите первое число: ");
+  int numberA = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Введите второе число: ");
+  int numberB = Convert.ToInt32(Console.ReadLine());
+
+  int Naturaldegree = degree(numberA, numberB);
+  Console.WriteLine("Ответ: " + Naturaldegree);
