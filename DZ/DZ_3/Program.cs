0b89d7862d5092b8892dfeafcd2f917c05ef6156
ZeN <zensmi@yandex.ru>
2022-08-17 14:57:37 +0300

    DZ

---
+++ DZ/DZ_3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая задаёт массив из N элементов и
+// выводит их на экран.
+
+Console.Write("Введите размер массива: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] numbers=new int[number];
+void FillArray(int[] array)
+{
+    for(int i =0; i < array.Length; i ++)
+    {
+        array[i] = new Random().Next(0,101);
+    }
+}
+void PrintArray(int[] array)
+{
+    for(int i =0; i < array.Length; i++)
+    {
+        Console.WriteLine(array[i]);
+    }
+}
+Console.WriteLine("Вывод: ");
+FillArray(numbers);
+PrintArray(numbers);
